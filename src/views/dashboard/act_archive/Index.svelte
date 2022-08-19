30cd9c9e30a9e89c3aa5172e7e06bb5cb30cfa3d
Patrick Bourgeois <patrickbourgeois@live.com>
2022-08-18 21:13:53 -0300

    OK

--- src/views/dashboard/act_archive/Index.svelte
+++ src/views/dashboard/act_archive/Index.svelte
@@ -1,5 +1,28 @@
 <script>
 export let currentRoute;
+export let params;
+
+
+import MsgCard from 'components/elements/MsgCard.svelte';
+
+import { Navigate } from 'svelte-router-spa';
+
+
+
+const resources = [
+	{
+		name: 'ACT Archive: Coaches',
+		to: '/my/act_archive/coaches',
+	},
+	{
+		name: 'ACT Archive: Hockey School Registrations',
+		to: '/my/act_archive/hockey_school_registrations',
+	},
+	{
+		name: 'ACT Archive: Spring Hockey Registrations',
+		to: '/my/act_archive/spring_hockey_registrations',
+	},
+];
 
 
 
@@ -9,4 +32,35 @@ export let currentRoute;
 <style>
 
 </style>
+
+
+
+<section class="section skinny-section" style="padding-bottom: 1.5rem !important;">
+
+	<div class="container is-fullwidth">
+
+		<MsgCard msg_show={true} msg_forever={true} msg_type={'error'} msg_text={'Helpers:'} />
+
+		<div class="columns is-variable is-mobile is-multiline is-centered">
+
+			{#each resources as resource}
+
+				<div class="column is-4-desktop is-4-tablet is-6-mobile">
+
+					<Navigate styles='button is-medium is-fullwidth' to='{resource.to}'>
+						<span>{resource.name}</span>
+					</Navigate>
+
+				</div>
+
+			{/each}
+
+		</div>
+
+	</div>
+
+</section>
+
+
+
 
